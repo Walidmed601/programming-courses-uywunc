b96df3b8ae5af36233ceaaefc1e2e0d4b114f2c7
Walidmed601 <medwalid601@gmail.com>
2021-06-26 13:33:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-uywunc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-uywunc)
---
+++ index.ts
@@ -0,0 +1,49 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let code1: number;
+let code2: number;
+code1 = 70;
+code2 = 30;
+console.log(code1 + code2);
+
+let num1: number;
+let num2: number;
+let num3: number;
+
+num1 = 10;
+num2 = 50;
+num3 = num1 / num2;
+console.log(num3);
+
+let num4: number;
+let num5: number;
+let num6: number;
+let num7: number;
+let num8: number;
+num4 = 10;
+num5 = 10;
+num6 = 10;
+num7 = 10;
+num8 = num4 + num5 + num6 + num7;
+console.log(num8);
+
+let text1, text2, text3, text4: string;
+text1 = 'walid';
+text2 = 'medjoudj';
+text3 = 'batna';
+text4 = text1 + text2 + text3;
+console.log(text4);
+
+let mynumber: number;
+mynumber = 100;
+mynumber = mynumber + 10;
+console.log(mynumber);
+let numb1, numb2, numb3: number;
+numb1 = 10;
+numb2 = 20;
+numb1 = numb3;
+numb2 = numb1;
+numb2 = numb3;
+console.log(numb1);
